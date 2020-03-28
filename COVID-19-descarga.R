1829508e118396d95e068bd484f634dc25a095a9
rubenfcasal <rubenfcasal@gmail.com>
2020-03-28 10:05:10 +0100

    Datos ISCIII

--- COVID-19-descarga.R
+++ COVID-19-descarga.R
@@ -27,16 +27,37 @@ for (f in files)
 f <- "serie_historica_acumulados.csv"
 download.file(paste0("https://covid19.isciii.es/resources/", f), f, mode="wb")
 
-acumulados <- read.csv(f, colClasses = c("factor", "Date", rep("integer", 4)))
+# Importar
+# --------
+
+acumulados <- read.csv(f, colClasses = c("character", "character", rep("integer", 4)))
+
+nota.texto <- acumulados[nrow(acumulados), 1]
+nota.texto
+acumulados <- acumulados[-nrow(acumulados), ]
+
+acumulados$Fecha <- as.Date(acumulados$Fecha, format = "%d/%m/%Y")
+names(acumulados)[1] <- "CCAA.ISO"
+# write.csv2(unique(acumulados$CCAA.ISO), file = "levels.csv")
+# https://www.iso.org/obp/ui/#iso:code:3166:ES
+CCAA.ISO <- read.csv2("CCAA.ISO.csv", colClasses = "character")
+acumulados$CCAA.ISO <- factor(acumulados$CCAA.ISO, levels = CCAA.ISO$COD_CCAA)
+iccaa <- match(as.character(acumulados$CCAA.ISO), CCAA.ISO$COD_CCAA)
+acumulados <- tibble::add_column(acumulados, CCAA = CCAA.ISO$DESC_CCAA[iccaa], .before = 1)
+acumulados$CCAA <- factor(acumulados$CCAA, levels = CCAA.ISO$DESC_CCAA)
+
 str(acumulados)
 
 # NA's
+# ----
 sapply(acumulados, function(x) sum(is.na(x)))
-which(is.na(acumulados$Fecha))
-acumulados <- acumulados[!is.na(acumulados$Fecha), ]
+# which(is.na(acumulados$Fecha))
+# acumulados <- acumulados[!is.na(acumulados$Fecha), ]
 acumulados[is.na(acumulados)] <- 0
 
-plot(acumulados)
+# plot(acumulados[, -(1:2)])
+attr(acumulados, "note") <- nota.texto
+attr(acumulados, "url") <- "https://covid19.isciii.es/resources/serie_historica_acumulados.csv"
 save(acumulados, file = "acumulados.RData")
 
 
