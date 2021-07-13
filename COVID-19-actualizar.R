670f945c35217567a7190f1d42aa682c86559a1d
rubenfcasal <rubenfcasal@gmail.com>
2021-07-13 23:40:32 +0200

    Datos actualizados a fecha 2021-07-09

--- COVID-19-actualizar.R
+++ COVID-19-actualizar.R
@@ -9,7 +9,7 @@ r <- HEAD(paste0("https://cnecovid.isciii.es/covid19/resources/", f))
 # r$headers$`content-length`
 unlist(r$headers[c("last-modified", "content-length")])
 #                  last-modified                  content-length 
-# "Thu, 08 Jul 2021 10:07:22 GMT"                      "25991217" 
+# "Tue, 13 Jul 2021 07:49:50 GMT"                      "26226589" 
 
 
 # Descargar 
